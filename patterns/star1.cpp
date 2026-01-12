6937160409913f84f4fd450de36460da2b5defec
AsherKingpin <asherkingpin@gmail.com>
2026-01-12 11:08:50 +0530

    star pattern

--- patterns/star1.cpp
+++ patterns/star1.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main(){
-    cout << "Hi";
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
     return 0;
 }
