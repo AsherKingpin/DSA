2cf2201610efd0589d7510d3e92f245bd3c5c7a3
AsherKingpin <asherkingpin@gmail.com>
2026-01-13 16:48:37 +0530

    number3

--- patterns/number3.cpp
+++ patterns/number3.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main(){
     for(int i = 5; i > 0; i--){
-        for(int j = 1; j > 0; j--){
-            cout << '*' << " ";
+        for(int j = 1; j <= i; j++){
+            cout << j << " ";
         }
         cout << endl;
     }
